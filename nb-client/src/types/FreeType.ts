a67591b9d6e1639594804e4fd05917e0b3300ec3
Jeongmin <srm0609@naver.com>
2022-07-09 08:16:42 +0900

    type 추가

---
+++ nb-client/src/types/FreeType.ts
@@ -0,0 +1,13 @@
+export interface FreeType {
+  id: number;
+  title: string;
+  context: string;
+  img1: string;
+  img2: string;
+  img3: string;
+  img4: string;
+  img5: string;
+  create_user: number;
+  create_date: string;
+  correction_date: string;
+}
