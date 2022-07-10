dffa0d047cb0bffbbb91e4e7d2d137b989209e80
Jeongmin <srm0609@naver.com>
2022-07-10 17:00:59 +0900

    Freetype변경

--- nb-client/src/Components/FreeWatch/FreeWatch.tsx
+++ nb-client/src/Components/FreeWatch/FreeWatch.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-interface BlogType {
-  id: number;
-  title: string;
-  context: string;
-  img1: string;
-  img2: string;
-  img3: string;
-  img4: string;
-  img5: string;
-  create_user: number;
-  create_date: string;
-  correction_date: string;
+import * as S from "./style";
+import { FreeType } from "../../types";
+
+interface FreeTypeProps {
+  freeWatch: FreeType;
 }
-interface BlogTypeProp {
-  freeWatch: BlogType;
-}
-const FreeWatch = ({ freeWatch }: BlogTypeProp) => {
-  return <>{freeWatch.title}</>;
+const FreeWatch = ({ freeWatch }: FreeTypeProps) => {
+  return (
+    <>
+      <S.Positioner>
+        <S.Board>
+          <S.Imgboard>
+            <S.Img>
+              <img alt="image" src={freeWatch.img1} />
+            </S.Img>
+          </S.Imgboard>
+          <S.Title>{freeWatch.title}</S.Title>
+          <S.Infobox>{freeWatch.context}</S.Infobox>
+        </S.Board>
+      </S.Positioner>
+    </>
+  );
 };
 
 export default FreeWatch;
--- nb-client/src/Components/Freeitem/FreeItem.tsx
+++ nb-client/src/Components/Freeitem/FreeItem.tsx
@@ -1,20 +1,8 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import * as S from "./style";
-interface BlogType {
-  id: number;
-  title: string;
-  context: string;
-  img1: string;
-  img2: string;
-  img3: string;
-  img4: string;
-  img5: string;
-  create_user: number;
-  create_date: string;
-  correction_date: string;
-}
-const FreeItem: React.FC<BlogType> = ({
+import { FreeType } from "../../types";
+const FreeItem: React.FC<FreeType> = ({
   id,
   title,
   context,
