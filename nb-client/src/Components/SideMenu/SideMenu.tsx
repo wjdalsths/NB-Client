f3ff492967a07e80c3d23d1af89f17a9ebbc974d
Jeongmin <srm0609@naver.com>
2022-07-14 16:01:51 +0900

    다른페이지 제한 추가

--- nb-client/src/Components/SideMenu/SideMenu.tsx
+++ nb-client/src/Components/SideMenu/SideMenu.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import * as S from "./style";
+import isLogin from "../../Utils/Libs/isLogin";
+
 const SideMenu = () => {
   const navigate = useNavigate();
   return (
@@ -12,19 +14,25 @@ const SideMenu = () => {
           </S.Icons>
           <span>자유</span>
         </S.Items>
-        <S.Items onClick={() => navigate(`/story`)}>
+        <S.Items
+          onClick={() => (isLogin() ? navigate(`/story`) : navigate("/"))}
+        >
           <S.Icons>
             <img src="Icons/book.png" alt="" />
           </S.Icons>
           <span>스토리</span>
         </S.Items>
-        <S.Items onClick={() => navigate(`/question`)}>
+        <S.Items
+          onClick={() => (isLogin() ? navigate(`/question`) : navigate("/"))}
+        >
           <S.Icons>
             <img src="Icons/document.png" alt="" />
           </S.Icons>
           <span>문의사항</span>
         </S.Items>
-        <S.Items onClick={() => navigate(`/notice`)}>
+        <S.Items
+          onClick={() => (isLogin() ? navigate(`/notice`) : navigate("/"))}
+        >
           <S.Icons>
             <img src="Icons/notice.png" alt="" />
           </S.Icons>
