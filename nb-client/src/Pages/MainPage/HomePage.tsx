2f5796f83967fbef3f1593efe0c06174c0465011
Jeongmin <srm0609@naver.com>
2022-07-14 15:35:34 +0900

    navigate 삭제

--- nb-client/src/Pages/MainPage/HomePage.tsx
+++ nb-client/src/Pages/MainPage/HomePage.tsx
@@ -3,13 +3,8 @@ import * as S from "./style";
 import Header from "../../Components/Header/Header";
 import SideBar from "../../Components/SideBar/SideBar";
 import FreeContents from "../../Components/FreeContents/FreeContents";
-import { useNavigate } from "react-router-dom";
-import isLogin from "../../Utils/Libs/isLogin";
 
 const HomePage = () => {
-  const navigate = useNavigate();
-  isLogin() ? navigate("/home") : navigate("/");
-
   return (
     <>
       <S.Wrapper>
