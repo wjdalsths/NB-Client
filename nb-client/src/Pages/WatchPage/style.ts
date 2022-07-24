ec82d1b3847c7e2e62da97836a8bd1aa141b08d2
Jeongmin <srm0609@naver.com>
2022-07-24 22:11:12 +0900

    style수정

--- nb-client/src/Pages/WatchPage/style.ts
+++ nb-client/src/Pages/WatchPage/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 90%;
+  width: 80%;
   margin-left: auto;
   margin-right: auto;
 `;
--- nb-client/src/Pages/WritePage/style.ts
+++ nb-client/src/Pages/WritePage/style.ts
@@ -3,16 +3,11 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  /* width: 1728px; */
-  width: 90%;
+  width: 80%;
   margin-left: auto;
   margin-right: auto;
-  /* @media (max-width: 1919px) {
-    width: 1376px;
-  } */
 `;
 export const Container = styled.div`
-  /* margin-right: 15rem; */
   display: flex;
   width: 100%;
 `;
