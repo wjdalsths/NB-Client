e1688717008ee58fcddce404619f51fd13810488
Jeongmin <srm0609@naver.com>
2022-08-04 15:46:30 +0900

    storytype추가

---
+++ nb-client/src/types/StoryType.ts
@@ -0,0 +1,8 @@
+export interface StoryType {
+  id: number;
+  title: string;
+  context: string;
+  create_user: number;
+  create_date: string;
+  correction_date: string;
+}
--- nb-client/src/types/index.ts
+++ nb-client/src/types/index.ts
@@ -1,1 +1,2 @@
 export * from "./FreeType";
+export * from "./StoryType";
