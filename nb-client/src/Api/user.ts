0706abc437df084962fbac9fe54c2271b7de6c9b
Jeongmin <srm0609@naver.com>
2022-08-22 18:28:20 +0900

    signup api 추가

--- nb-client/src/Api/user.ts
+++ nb-client/src/Api/user.ts
@@ -10,6 +10,19 @@ export const signin = async (email: string, password: string) => {
   } catch (e: any) {}
 };
 
+export const signup = async (name: string, email: string, password: string) => {
+  try {
+    const { data } = await customAxios.post("/User/", {
+      name: name,
+      email: email,
+      password: password,
+    });
+    return { data };
+  } catch (e: any) {
+    console.log(e.message);
+  }
+};
+
 export const getUser = async (id: string | null) => {
   try {
     const { data } = await customAxios.get(`/User/${id}`);
